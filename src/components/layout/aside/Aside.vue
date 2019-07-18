3f86a00900d712b3e0005907f3d369bbfe1c9152
caojun <caojun@finstone.com.cm>
2019-07-18 15:36:06 +0800

    ..

--- src/components/layout/aside/Aside.vue
+++ src/components/layout/aside/Aside.vue
@@ -1,31 +1,40 @@
 <template>
-  <el-menu
-    :default-active="$route.path"
-    class="el-menu-vertical-demo"
-    mode="vertical"
-    text-color="#fff"
-    router
-    :collapse-transition="true"
-    @select="selectMenu"
-    active-text-color="#1890ff"
-    backgroundColor="#001529">
-    <template v-for="(item,index) in $store.getters.menus">
-      <el-submenu v-if="item.children.length>0" :key="item.id" :index="index+''">
-        <template slot="title">
+  <div>
+    <div class="logo-name">
+      <h2>后台管理系统</h2>
+    </div>
+    <el-menu
+      :default-active="$route.path"
+      class="el-menu-vertical-demo"
+      mode="vertical"
+      text-color="#fff"
+      router
+      :collapse-transition="true"
+      @select="selectMenu"
+      active-text-color="#1890ff"
+      backgroundColor="#001529">
+      <template v-for="(item,index) in $store.getters.menus">
+        <el-submenu v-if="item.children.length>0" :key="item.id" :index="index+''">
+          <template slot="title">
+            <i :class="item.iconClass"></i>
+            <span slot="title">{{item.name}}</span>
+          </template>
+          <template v-for="child in item.children">
+            <el-menu-item :index="child.path" :key="child.id">
+              <i :class="child.iconClass"></i>
+              <span slot="title">{{ child.name }}</span>
+            </el-menu-item>
+          </template>
+        </el-submenu>
+        <el-menu-item :index="item.path" v-else :key="item.id">
+          <i :class="item.iconClass"></i>
           <span slot="title">{{item.name}}</span>
-        </template>
-        <template v-for="child in item.children">
-          <el-menu-item :index="child.path" :key="child.id">
-            <span slot="title">{{ child.name }}</span>
-          </el-menu-item>
-        </template>
-      </el-submenu>
-      <el-menu-item :index="item.path" v-else :key="item.id">
-        <span slot="title">{{item.name}}</span>
-      </el-menu-item>
-    </template>
+        </el-menu-item>
+      </template>
 
-  </el-menu>
+    </el-menu>
+  </div>
+
 </template>
 
 <script>
@@ -43,5 +52,19 @@ export default {
 </script>
 
 <style scoped>
+
+  .logo-name {
+    background-color: #03152A !important;
+  @extend %w100;
+    font-weight: 300;
+    z-index: 999;
+  }
+  .logo-name h2 {
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+  }
 
 </style>
--- src/store/modules/menus.js
+++ src/store/modules/menus.js
@@ -9,10 +9,10 @@ const mutations = {
 const actions = {
   getMenus ({commit}) {
     const temp = [
-      {id: 1, path: '/system', name: '系统管理', children: [{id: 11, path: '/system/user', name: '用户管理', children: []}, {id: 12, path: '/system/role', name: '角色管理', children: []}, {id: 13, path: '/system/menu', name: '菜单管理', children: []}]},
-      {id: 2, path: '/plan', name: '计划管理', children: []},
-      {id: 3, path: '/project', name: '项目管理', children: []}
-    ] // 模拟请求后的数据
+      {id: 1, path: '/system', iconClass: 'el-icon-s-tools', name: '系统管理', children: [{id: 11, path: '/system/user', iconClass: 'el-icon-s-custom', name: '用户管理', children: []}, {id: 12, path: '/system/role', iconClass: 'el-icon-user', name: '角色管理', children: []}, {id: 13, path: '/system/menu', iconClass: 'el-icon-s-order', name: '菜单管理', children: []}]},
+      {id: 2, path: '/plan', iconClass: 'el-icon-s-flag', name: '计划管理', children: []},
+      {id: 3, path: '/project', iconClass: 'el-icon-s-finance', name: '项目管理', children: []}
+    ] // 模拟请求后的数
     state.menus = temp
     commit('getMenus')
   }
